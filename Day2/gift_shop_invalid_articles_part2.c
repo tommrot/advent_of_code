178c8e710a564db48c9fc282e4eeb71fc6b8c4cd
tommrot <tommasorotelli12@gmail.com>
2026-06-21 17:39:01 +0200

    implemented part2 of day2

---
+++ Day2/gift_shop_invalid_articles_part2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void invalid_id_in_range(long long start, long long end, long long *sum);
+int is_invalid_id(long long n);
+
+int main(void){
+    long long sum = 0;
+    FILE *f_ptr = fopen("id_range_list.txt", "r");
+    if (f_ptr == NULL) return EXIT_FAILURE;
+    char *row = malloc(600 *sizeof(char));
+    if (fgets(row, 600, f_ptr)){
+        char *token = strtok(row, "-");
+        while (token != NULL){
+            long long start = atoll(token);
+            token = strtok(NULL, ",");
+            if (token == NULL) return EXIT_FAILURE;
+            long long end = atoll(token);
+            invalid_id_in_range(start, end, &sum);
+            token = strtok(NULL, "-");
+        }
+    }
+    printf("sum of invalid id: %lld\n", sum);
+}
+
+
+
+void invalid_id_in_range(long long start, long long end, long long *sum){
+    for (long long i = start; i <= end; i++){
+        if (is_invalid_id(i)) *sum += i;
+    }
+}
+
+
+int is_invalid_id(long long n){
+    char buf[32];
+    sprintf(buf, "%lld", n);
+    int len = strlen(buf);
+    for (int L = 1; L < len; L++){
+        if (len % L != 0) continue;
+        int intervals = len / L;
+        int looks_invalid = 1;
+        for (int i = 1; i < intervals && looks_invalid; i++){
+            for (int s = 0; s < L; s++){
+                if (buf[s] != buf[i * L + s]){looks_invalid = 0; break;}
+            }
+        }
+        if(looks_invalid) return 1;
+    } 
+    return 0;
+}
