cf9c7a55bdba33bea588f5c88e97714368fb7fc7
tommrot <tommasorotelli12@gmail.com>
2026-06-26 16:06:46 +0200

    implemented right part 2 of day 5 whith merge intervals

--- Day5/part2.c
+++ Day5/part2.c
@@ -2,52 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+typedef struct Ingredient{
+    char *row;
+    long long start;
+    long long end;
+} Ingredient;
+
+
 #define ROWS_NUM 177
 #define RANGE_SIZE 100
 
-int count_id(char *row, char **fresh_found, int fresh_found_counter);
-int check_duplicates(long long num, char **fresh_found, int fresh_found_counter);
+
+int compare_starts(const void *a, const void *b);
+int merge_intervals(Ingredient *items);
 
 
 int main(void){
-    long long total_fresh_id = 0;
-    char *fresh_found[ROWS_NUM];
+    long long total = 0;
+    Ingredient items[ROWS_NUM];
+    char buff[RANGE_SIZE];
+
     FILE *file_ptr = fopen("ingredients.txt", "r");
-    if (file_ptr == NULL) exit(0);
-
-    char *row = malloc(RANGE_SIZE);
-
     for (int i = 0; i < ROWS_NUM; i++){
-        fgets(row, RANGE_SIZE, file_ptr);
-        total_fresh_id += count_id(row, fresh_found, i);
-        fresh_found[i] = malloc(RANGE_SIZE);
-        strcpy(fresh_found[i], row);
+        items[i].row = malloc(RANGE_SIZE);
+        fgets(buff, RANGE_SIZE, file_ptr);
+        strcpy(items[i].row, buff);
+        items[i].start = atoll(strtok(buff, "-"));
+        items[i].end = atoll(strtok(NULL, "\n"));
     }
-    printf("total fresh ingredients are: %lld\n", total_fresh_id);
+    fclose(file_ptr);
+    qsort(items, ROWS_NUM, sizeof(Ingredient), compare_starts);
+
+    int counter = merge_intervals(items);
+    for (int i = 0; i < counter; i++){
+        total += (items[i].end - items[i].start + 1);
+    }
+    printf("ingredients are: %lld\n", total);
+    
 }
 
 
-int count_id(char *row, char **fresh_found, int fresh_found_counter){
-    long long counter = 0;
-    char *copy = malloc(RANGE_SIZE);
-    strcpy(copy, row);
-    long long start = atoll(strtok(copy, "-"));
-    long long end = atoll(strtok(NULL, "\n"));
 
-    for (long long i = start; i <= end; i++){
-        if (check_duplicates(i, fresh_found, fresh_found_counter)) counter++;
-    }
-    return counter;
+int compare_starts(const void *a, const void *b){   //compare function inside qsort is declared as int compare(const void *a, const void *b); 
+    const Ingredient *a_ptr = (const Ingredient *) a;
+    const Ingredient *b_ptr = (const Ingredient *) b;
+    
+    if (a_ptr->start > b_ptr->start) return 1;
+    if (a_ptr->start < b_ptr->start) return -1;
+    return 0;
 }
 
 
-int check_duplicates(long long num, char **fresh_found, int fresh_found_counter){
-    for (int i = 0; i < fresh_found_counter; i++){
-        char *copy = malloc(RANGE_SIZE);
-        strcpy(copy, fresh_found[i]);
-        long long founded_start = atoll(strtok(copy, "-"));
-        long long founded_end = atoll(strtok(NULL, "\n"));
-        if (founded_start <= num && num <= founded_end) return 0;
+int merge_intervals(Ingredient *items){
+    int j = 0;
+    for (int i = 1; i < ROWS_NUM; i++){
+        if (items[j].end >= items[i].start){
+            if (items[i].end > items[j].end) items[j].end = items[i].end;
+            free(items[i].row);
+            items[i].row = NULL;
+        } else {
+            j++;
+            items[j] = items[i];
+        }
     }
-    return 1;
+    return j + 1;  //j is index, but we want to pass length
 }
